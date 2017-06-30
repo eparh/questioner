8e642234ae8e408f26a5757df42744f7b8a5a27e
pylis <egenpark@gmail.com>
2017-06-30 17:23:57 +0300

    rateLimit

---
+++ server/middlewares/rateLimit/rateLimit.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const ratelimit = require('koa-ratelimit');
+const Redis = require('ioredis');
+
+module.exports = ratelimit({
+  db: new Redis(),
+  duration: 60000,
+  errorMessage: 'Sometimes You Just Have to Slow Down.',
+  id: (ctx) => ctx.ip,
+  headers: {
+    remaining: 'Rate-Limit-Remaining',
+    reset: 'Rate-Limit-Reset',
+    total: 'Rate-Limit-Total'
+  },
+  max: 100
+});
--- server/web/routes/user.js
+++ server/web/routes/user.js
@@ -4,6 +4,7 @@ const BaseRoute = require('./base');
 const passport = require('passport');
 const validateLoginInfo = require('../validators/user/validateLoginInfo');
 const validateRegisterInfo = require('../validators/user/validateRegisterInfo');
+const ratelimit = require('../../middlewares/rateLimit/rateLimit');
 
 class UserRoute extends BaseRoute {
   constructor({ userController }) {
@@ -14,8 +15,8 @@ class UserRoute extends BaseRoute {
     const self = this;
     const { validator } = self;
 
-    router.post('/register', validator(validateRegisterInfo), self.registerHandler('register'));
-    router.post('/login', validator(validateLoginInfo), passport.authenticate('local', {
+    router.post('/register', ratelimit, validator(validateRegisterInfo), self.registerHandler('register'));
+    router.post('/login', ratelimit, validator(validateLoginInfo), passport.authenticate('local', {
       failureRedirect: '/login',
       successRedirect: '/questions'
     }));
